ed86e6b7406204077ca5b3d2deb892febc7e3dce
darth <kuragari.ch@gmail.com>
2014-07-13 12:59:46 +0200

    Finished first version of the script

---
+++ pyvol.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# econsing: utf-8
+
+from argparse import ArgumentParser
+from colorama import init, Fore
+from commands import getstatusoutput
+
+
+def print_status( status, nchr=10 ):
+    dchr = int( status[ 'vol' ] ) * nchr / 100
+    disp = '{0:>3}% [{1}{2}] {3}'.format(
+        status[ 'vol' ],
+        '#' * dchr,
+        ' ' * ( nchr - dchr ),
+        Fore.RED + 'mute' + Fore.RESET if status[ 'status' ] == 'off' else ''
+    )
+    print( disp )
+
+
+def get_status():
+    _, output = getstatusoutput( 'amixer -D pulse' )
+    output    = output.split( '\n' )
+    output    = [ line 
+                     for line in output 
+                     if 'Front Left: Playback' in line 
+                ][ 0 ]
+    output    = [ nn.strip( ' ' ).strip( ']' ).strip( '%' )
+                      for nn in output.split( '[' )[ 1: ] 
+                ]
+    return { 'vol': output[ 0 ], 'status': output[ 1 ] }
+
+
+def set_volume( action ):
+    if action not in ( 'up', 'down', 'mute' ):
+         return
+    if action == 'up':
+         _, output = getstatusoutput( 'amixer -D pulse set Master 3276+' )
+    if action == 'down':
+         _, output = getstatusoutput( 'amixer -D pulse set Master 3276-' )
+    if action == 'mute':
+         _, output = getstatusoutput( 'amixer -D pulse set Master toggle' )
+    return output
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument( 'action', help = 'up, down, mute or ask', type = str )
+
+    args = parser.parse_args()
+    args.action = args.action.lower()
+    init()
+
+    if args.action != 'ask':
+        set_volume( args.action )
+    
+    print_status( get_status() )
+
+
+if __name__ == '__main__':
+    main()
